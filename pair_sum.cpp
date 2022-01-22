3238132e4bff40ec186e3f32fcc9e4591d2b8282
venkat05091990 <venkat05091990@gmail.com>
2022-01-22 09:15:50 +0530

    Create pair_sum.cpp

---
+++ pair_sum.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+vector<int> pairSum(vector<int> arr, int Sum){
+
+	unordered_set<int> s;
+	vector<int> result;
+
+    cout<<endl;
+	for(int i=0;i<arr.size();i++)
+	{
+		int comp = Sum - arr[i];
+
+		if(s.find(comp)!=s.end())
+		{
+			result.push_back(arr[i]);
+			result.push_back(comp);
+			return result;
+		}
+        
+        cout<<"iteration "<<i<<endl; 
+        for(int num: s){
+            cout<<num;            
+        }
+        cout<<endl;
+
+		s.insert(arr[i]);
+	}
+	return result;
+}
+
+int main()
+{
+    cout<<"Hello World";
+    
+    vector<int> v = {1,2,3,4,5,6,7};
+    
+    vector<int> v_res = pairSum(v, 10);
+    
+    cout<<v_res[0]<<v_res[1]<<endl;
+    
+    return 0;
+}
