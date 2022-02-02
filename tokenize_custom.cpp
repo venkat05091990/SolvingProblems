e45f7a512e731058a22b9feae2e08ece779f1412
Venkat Rao <venkat05091990@gmail.com>
2022-02-02 09:05:08 +0530

    Create tokenize_custom.cpp

---
+++ tokenize_custom.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+char * mystrtok(char *str, char delim){
+    static char *input = NULL;
+    
+    if(str!=NULL){
+        input = str;
+    }
+    if(input==NULL){
+        return NULL;
+    }
+    
+    // dynamic array
+    char *token = new char[strlen(input)+1];
+    int i =0;
+    
+    for(; input[i]!='\0'; i++){
+        if(input[i]!=delim){
+            token[i] = input[i];
+        }
+        else
+        {
+            token[i] = '\0';
+            input = input +i + 1;
+            return token;
+        }
+    }
+    
+    token[i] = '\0';
+    
+    // reset input
+    input = NULL;
+    return token;
+}
+
+
+int main()
+{
+    char s[1000];
+    cin.getline(s, 1000);
+    
+    //strtok
+    char *token = mystrtok(s, ' ');
+    
+    while(token!=NULL){
+        cout<<token<<endl;
+        token = mystrtok(NULL, ' ');
+    }
+    return 0;
+}
